7f0752e732bd2e43c041c6183c54a0dcaf3d9c72
David Boedigheimer <stealth259@gmail.com>
2021-06-07 15:28:28 -0700

    Add put, post, edit, and delete.

--- CretaceousClient/Controllers/HomeController.cs
+++ CretaceousClient/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,18 +13,7 @@ namespace CretaceousClient.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return RedirectToAction("Index", "Animals");
         }
     }
 }
